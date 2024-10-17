64bef9bef2aa749c51d7d2cc3cf86607860800e9
paulorobertorodrigues <pauloroberto23061985@gmail.com>
2024-10-17 09:12:12 -0300

    INIT:Iniciando a API

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+app.listen(3001, () => {
+    console.log("Servidor rodando na porta: 3001");
+});
