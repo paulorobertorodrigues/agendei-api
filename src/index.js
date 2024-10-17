208d5fee2f3d16c6eb1d3eb155bbf1463bbbd92c
paulorobertorodrigues <pauloroberto23061985@gmail.com>
2024-10-17 10:03:23 -0300

    INIT: Banco de dados e construção da API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
+import router from "./routes.js";
 
 const app = express();
 
 app.use(cors());
+app.use(router);
+
 
 app.listen(3001, () => {
     console.log("Servidor rodando na porta: 3001");
